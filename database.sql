95b00dce546c1b16e4694888829de3a04c3a784e
Kevin Shih <kevinmkshih@gmail.com>
2021-04-09 11:52:14 -0700

    fix DB

--- database.sql
+++ database.sql
@@ -11,12 +11,12 @@ create table `items`
 );
 
 
-insert into `items` (name, quantity, description) values ('test name', 3, 'this is a test object');
-insert into `items` (name, quantity, description) values ('test name', 4, 'this is a test object');
-insert into `items` (name, quantity, description) values ('test name', 5, 'this is a test object');
-insert into `items` (name, quantity, description) values ('test name', 6, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name1', 3, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name2', 4, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name3', 5, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name4', 6, 'this is a test object');
 
-
+drop table if exists api_access;
 CREATE TABLE `api_access` (
     name varchar(256) not null,
     count int
@@ -28,18 +28,9 @@ insert into `api_access` values ("/getApiCount", 0);
 insert into `api_access` values ("/itemsid", 0);
 
 
-create table api_access_record
-(
-    id int auto_increment,
-    api_id int not null,
-    access_count int default 0 not null,
-    constraint api_access_record_pk primary key (id),
-    constraint api_access_record_items_id_fk
-        foreign key (api_id) references items (id)
-            on update cascade on delete cascade
-);
 
-drop table if exist `users`;
+
+drop table if exists `users`;
 
 create table users (
     id int auto_increment primary key,
