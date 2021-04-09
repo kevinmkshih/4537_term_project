3c62315406ed6e65957b08b6fed7b19249f9d61a
Kevin Shih <kevinmkshih@gmail.com>
2021-04-09 10:44:35 -0700

    database update

--- database.sql
+++ database.sql
@@ -12,13 +12,20 @@ create table `items`
 
 
 insert into `items` (name, quantity, description) values ('test name', 3, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name', 4, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name', 5, 'this is a test object');
+insert into `items` (name, quantity, description) values ('test name', 6, 'this is a test object');
+
 
 CREATE TABLE `api_access` (
     name varchar(256) not null,
     count int
 );
 
-insert into `api_access` values ("/allItems", 5);
+insert into `api_access` values ("/allItems", 0);
+insert into `api_access` values ("/itemsName", 0);
+insert into `api_access` values ("/getApiCount", 0);
+insert into `api_access` values ("/itemsid", 0);
 
 
 create table api_access_record
@@ -30,4 +37,12 @@ create table api_access_record
     constraint api_access_record_items_id_fk
         foreign key (api_id) references items (id)
             on update cascade on delete cascade
+);
+
+drop table if exist `users`;
+
+create table users (
+    id int auto_increment primary key,
+    username varchar(256),
+    password varchar(256)
 );
