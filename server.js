b196519b4ae5eb3c2c560cfcefbd83b2954cce04
Kevin Shih <kevinmkshih@gmail.com>
2021-04-09 10:44:23 -0700

    api's working

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(`${endpointRoot}/public`, express.static("public"));
 
 const mysql = require("mysql");
 
+const promise = require("promise");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
 const url = require("url");
@@ -30,8 +31,7 @@ db.connect((err) => {
 })
 //#endregion
 
-
-//#region Get requests item
+//#region Items
 
 app.get(endpointRoot + "/allItems", (req, res) => {
     addApiCount("/allItems");
@@ -46,18 +46,39 @@ app.get(endpointRoot + "/allItems", (req, res) => {
             if (results != 0) {
                 res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
                 res.end(JSON.stringify(results));
-            // if results is empty
+                // if results is empty
             } else {
                 res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
                 res.end("No inventory found.")
             }
-    }
+        }
     })
+})
+
+app.delete(endpointRoot + "/deleteItemId/:id", (req, res) => {
+    let sql = `DELETE FROM items WHERE id = ${req.params.id}`;
+    let itemFound = searchItem(req.params.id);
+    itemFound.then((result) => {
+        if (result === 200) {
+            db.query(sql, (err, result) => {
+                res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+                res.end("Item deleted");
+            })
+        } else if (result === 404) {
+            res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+            res.end("Item not found");
+        } else {
+            res.writeHead(500, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+            res.end(result.message);
+
+        }
+    })
+
+
 })
 
 app.get(endpointRoot + "/itemsid", (req, res) => {
     addApiCount("/itemsid");
-    console.log("ok")
     let q = url.parse(req.url, true);
     let id = q.query['id'];
     let sql = `SELECT * FROM items WHERE id = ${id}`;
@@ -70,9 +91,8 @@ app.get(endpointRoot + "/itemsid", (req, res) => {
             // if not empty result from database
             if (results.length != 0) {
                 res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
-                let resultJson = JSON.stringify(results);
                 res.end(JSON.stringify(results));
-            // if empty result from database
+                // if empty result from database
             } else {
                 res.writeHead(404, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
                 res.end("Item not found");
@@ -82,14 +102,93 @@ app.get(endpointRoot + "/itemsid", (req, res) => {
     })
 })
 
-app.get(endpointRoot + "/itemsName", (req, res) => {
-    addApiCount("/itemsName")
+app.put(endpointRoot + "/updateItemStock", (req, res) => {
     let q = url.parse(req.url, true);
+    let id = q.query['id'];
+    let quantity = q.query['quantity'];
+    let getAmountSQL = `update items set quantity = ${quantity} where id = ${id}`;
+    db.query(getAmountSQL, (err, results) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+            console.log(err.message);
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+            res.end("Quantity successfully updated.")
+        }
+    })
 })
+
+app.put(endpointRoot + "/updateName", (req, res) => {
+    let body = '';
+    let jsonBody;
+    req.on("data", (chunk) => {
+        if (chunk != null) {
+            body += chunk;
+        }
+    })
+    req.on("end", () => {
+        jsonBody = JSON.parse(body);
+        console.log(jsonBody.name)
+
+        sql = `UPDATE items SET name = '${jsonBody.name}' WHERE id = ${jsonBody.id}`
+        db.query(sql, (err, result) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+                res.end(err.message)
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+                res.end("Name successfully updated")
+            }
+        })
+    })
+})
+
+app.post(endpointRoot + "/addNewItem", (req, res) => {
+    let body = "";
+    let jsonBody;
+    req.on("data", (chunk) => {body += chunk;})
+    req.on("end", () => {
+        jsonBody = JSON.parse(body);
+        let quantity;
+        let description;
+        jsonBody.quantity = null ? quantity = 0 : quantity = jsonBody.quantity;
+        jsonBody.description = null ? description = '' : description = jsonBody.description;
+        let sql = `INSERT INTO items (name, quantity, description) VALUES ("${jsonBody.name}", ${quantity}, "${description}")`;
+        db.query(sql, (err, results) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+                res.end(err.message);
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+                res.end("Name successfully added")
+            }
+        })
+    })
+})
+
+
+function searchItem(id) {
+    return new Promise((resolve, reject) => {
+        let searchSQL = `SELECT * FROM items WHERE id = ${id}`;
+        db.query(searchSQL, (err, results) => {
+            if (err) {
+                console.log(err.message);
+                reject(err);
+            }
+            if (results != 0) {
+                resolve(200);
+            } else {
+                resolve(404);
+            }
+        })
+    })
+}
+
+
 //#endregion
 
 //#region api history
-app.get(endpointRoot + "/", (req, res) => {
+app.get(endpointRoot + "/getApiCount", (req, res) => {
     let sql = "SELECT * FROM api_access"
     db.query(sql, (err, results) => {
         if (err) {
@@ -105,7 +204,7 @@ function addApiCount(route) {
     let sql = `update api_access set count = count + 1 where name = '${route}'`;
     db.query(sql, (err, results) => {
         if (err) {
-            console.log(err.message);
+
             throw err;
         } else {
             console.log("added api access count");
